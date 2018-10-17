78789f5dcba5151fc07827dd92b76d99b7a4dc82
Nicholas Nazario <nazario9847@gmail.com>
2018-10-17 11:31:17 -0400

    -Table with Dyanamic Sorting -Using Dummy Data

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,13 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
-}]);
+.controller('View1Ctrl', function($scope) {
+  //Sample Data
+  $scope.players = [
+    {fname:"Nick", lname:"Nazario", pos:"WR", team:"Chill End"}
+    , {fname:"Gerb", lname:"Vande Hei", pos:"RB", team:"Warm End"}
+    , {fname:"Colin", lname:"Williamson", pos:"QB", team:"Warm End"}
+    , {fname:"Crisp", lname:"McCulloch", pos:"WR", team:"Warm End"}
+  ];
+  $scope.sortType = 'lname';
+});
